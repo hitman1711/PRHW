f244f1743984da9e79158e8a945528ecda4d577d
partyBegins <coding4life@icloud.com>
2017-03-29 22:02:11 +0300

    Initial Commit

---
+++ PageRankiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PageRankiOS
+//
+//  Created by Артур Сагидулин on 29.03.17.
+//  Copyright © 2017 Артур Сагидулин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
